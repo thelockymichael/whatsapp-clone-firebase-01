16af293ed5e2753512f8005da6ace49c0bcd8608
Michael Lock <thelockymichael@gmail.com>
2021-03-20 00:19:55 +0200

    firestore integration

--- navigation/HomeStack.js
+++ navigation/HomeStack.js
@@ -2,12 +2,52 @@ import React from 'react'
 import {createStackNavigator} from '@react-navigation/stack'
 import HomeScreen from '../screens/HomeScreen'
 
+import {IconButton} from 'react-native-paper'
+
+import AddRoomScreen from '../screens/AddRoomScreen'
+
 const Stack = createStackNavigator()
+
+// create two new instances
+const ChatAppStack = createStackNavigator()
+const ModalStack = createStackNavigator()
+
+const ChatApp = () => {
+  return (
+    <ChatAppStack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#6646ee',
+        },
+        headerTintColor: '#ffffff',
+        headerTitleStyle: {
+          fontSize: 22,
+        },
+      }}
+    >
+      <ChatAppStack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={({navigation}) => ({
+          headerRight: () => (
+            <IconButton
+              icon="message-plus"
+              size={28}
+              color="#ffffff"
+              onPress={() => navigation.navigate('AddRoom')}
+            />
+          ),
+        })}
+      />
+    </ChatAppStack.Navigator>
+  )
+}
 
 export default HomeStack = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
-    </Stack.Navigator>
+    <ModalStack.Navigator mode="modal" headerMode="none">
+      <ModalStack.Screen name="ChatApp" component={ChatApp} />
+      <ModalStack.Screen name="AddRoom" component={AddRoomScreen} />
+    </ModalStack.Navigator>
   )
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,21 +1,67 @@
-import React, {useContext} from 'react'
-import {View, StyleSheet} from 'react-native'
+import React, {useContext, useState, useEffect} from 'react'
+import {View, StyleSheet, FlatList} from 'react-native'
+import {List, Divider} from 'react-native-paper'
 import {Title} from 'react-native-paper'
 import {AuthContext} from '../navigation/AuthProvider'
 import FormButton from '../components/FormButton'
+// after other import statements
+import * as firebase from 'firebase'
+import 'firebase/firestore'
 
-export default HomeScreen = () => {
+import Loading from '../components/Loading'
+
+export default HomeScreen = ({navigation}) => {
   const {user, logout} = useContext(AuthContext)
+  const [threads, setThreads] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const unsubscribe = firebase
+      .firestore()
+      .collection('THREADS')
+      .onSnapshot((querySnapshot) => {
+        const threads = querySnapshot.docs.map((documentSnapshot) => {
+          return {
+            _id: documentSnapshot.id,
+            // give defaults
+            name: '',
+            ...documentSnapshot.data(),
+          }
+        })
+
+        setThreads(threads)
+
+        if (loading) {
+          setLoading(false)
+        }
+      })
+
+    /**
+     * unsubscribe listener
+     */
+    return () => unsubscribe()
+  }, [])
+
+  if (loading) {
+    return <Loading />
+  }
 
   return (
     <View style={styles.container}>
-      <Title>Home Screen</Title>
-      <Title>All chat rooms will be listed here</Title>
-      <Title>{user.uid}</Title>
-      <FormButton
-        modeValue="contained"
-        title="Logout"
-        onPress={() => logout()}
+      <FlatList
+        data={threads}
+        keyExtractor={(item) => item._id}
+        ItemSeparatorComponent={() => <Divider />}
+        renderItem={({item}) => (
+          <List.Item
+            title={item.name}
+            description="Item description"
+            titleNumberOfLines={1}
+            titleStyle={styles.listTitle}
+            descriptionStyle={styles.listDescription}
+            descriptionNumberOfLines={1}
+          />
+        )}
       />
     </View>
   )
@@ -25,7 +71,11 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#f5f5f5',
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  },
+  listTitle: {
+    fontSize: 22,
+  },
+  listDescription: {
+    fontSize: 16,
   },
 })
