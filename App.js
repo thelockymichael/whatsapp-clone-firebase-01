6f27632447bf1296da0c3ff0c31af09ccba57eb4
Michael Lock <thelockymichael@gmail.com>
2021-03-23 11:37:30 +0200

    test attempt 05

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
 // import * as apiKeys from './config/keys'
 // import {StatusBar} from 'expo-status-bar'
 import React from 'react'
-// import {StyleSheet, Text, View} from 'react-native'
+import {StyleSheet, Text, View} from 'react-native'
 // import Providers from './navigation'
 
 export default App = () => {
