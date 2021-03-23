d18932cae33b0a07272ca171a375c90963ec6ec7
Michael Lock <thelockymichael@gmail.com>
2021-03-23 11:27:05 +0200

    test attempt 04

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
-import * as firebase from 'firebase'
-import * as apiKeys from './config/keys'
-import {StatusBar} from 'expo-status-bar'
+// import * as firebase from 'firebase'
+// import * as apiKeys from './config/keys'
+// import {StatusBar} from 'expo-status-bar'
 import React from 'react'
-import {StyleSheet, Text, View} from 'react-native'
-import Providers from './navigation'
+// import {StyleSheet, Text, View} from 'react-native'
+// import Providers from './navigation'
 
 export default App = () => {
   return (
